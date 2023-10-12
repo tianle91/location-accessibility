cf95747c4ee40735b46ad3a466fa0c5542081878
Tianle Chen <tianlechen@gmail.com>
2023-10-12 02:13:21 +0000

    now can select home/work and isochrone types

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from typing import Optional
 
 import folium
+import pandas as pd
 import streamlit as st
 from folium.map import Icon
 from streamlit_folium import folium_static
 from streamlit_js_eval import get_geolocation
 
 from src.geolocation import (
+    ISOCHRONE_PROFILES,
     Coords,
     get_geocode_top_hit,
     get_isochrones,
@@ -15,65 +17,114 @@ from src.geolocation import (
 
 MIN_ADDRESS_LENGTH = 5
 
-center_coords: Optional[Coords] = None
-address_str = st.text_input(label="Address")
-is_near_me = st.checkbox("Near me")
+ISOCHRONE_OPTIONS = {
+    # {display_name: (profile_name, display_color) for k in ISOCHRONE_PROFILES}
+    "Driving": ("driving-car", "green"),
+    "Cycling": ("cycling-regular", "blue"),
+    "Walking": ("foot-walking", "orange"),
+    "Wheelchair": ("wheelchair", "red"),
+}
+if set([v[0] for v in ISOCHRONE_OPTIONS.values()]) != set(ISOCHRONE_PROFILES):
+    raise ValueError(
+        f"ISOCHRONE_OPTIONS keys: {ISOCHRONE_OPTIONS.keys()} "
+        f"must match ISOCHRONE_PROFILES: {ISOCHRONE_PROFILES}"
+    )
+
+
+center_address_str = st.text_input(label="Center Address")
+home_address_str = st.text_input(label="Home Address")
+work_address_str = st.text_input(label="Work Address")
 
 browser_coords: Optional[Coords] = None
-if is_near_me:
+if st.checkbox("Near me"):
     browser_coords = parse_get_geolocation(get_geolocation())
-st.write(browser_coords)
 
-center_coords: Optional[Coords] = None
-if len(address_str) > 0:
-    if len(address_str) > MIN_ADDRESS_LENGTH:
-        query_params = {"text": address_str}
-        if browser_coords is not None:
-            query_params.update(
-                {
-                    "focus.point.lon": browser_coords.longitude,
-                    "focus.point.lat": browser_coords.latitude,
-                }
-            )
-            st.success("Using location from Browser location and Address field")
-        else:
-            st.success("Using location from Address field")
-        center_coords = get_geocode_top_hit(**query_params)
-    else:
+
+def get_coords_from_text(
+    text: str, browser_coords: Optional[Coords], warning_text_type: str = ""
+) -> Optional[Coords]:
+    if len(text) == 0:
+        return None
+    if len(text) < MIN_ADDRESS_LENGTH:
         st.warning(
-            f"Entered address is too short (<{MIN_ADDRESS_LENGTH}) and will be ignored"
+            f"Ignoring {warning_text_type} since entered text length <{MIN_ADDRESS_LENGTH}"
         )
+        return browser_coords
+    query_params = {"text": text}
+    if browser_coords is not None:
+        query_params.update(
+            {
+                "focus.point.lon": browser_coords.longitude,
+                "focus.point.lat": browser_coords.latitude,
+            }
+        )
+        st.success(f"Searching for {warning_text_type} near browser location.")
+    return get_geocode_top_hit(**query_params)
+
+
+center_coords = get_coords_from_text(
+    text=center_address_str,
+    browser_coords=browser_coords,
+    warning_text_type="Center Address",
+)
+home_coords = get_coords_from_text(
+    text=home_address_str,
+    browser_coords=browser_coords,
+    warning_text_type="Home Address",
+)
+work_coords = get_coords_from_text(
+    text=work_address_str,
+    browser_coords=browser_coords,
+    warning_text_type="Work Address",
+)
 
 if center_coords is None and browser_coords is not None:
     center_coords = browser_coords
-    st.success("Using location from Browser location")
-if center_coords is None:
-    st.error("No location provided")
-
-st.write(center_coords)
 
 if center_coords is not None:
-    center_lat_lon = (center_coords.latitude, center_coords.longitude)
-    bound_lat_lons = [
-        center_lat_lon,
-    ]
+    bound_lat_lons = []
 
+    center_lat_lon = (center_coords.latitude, center_coords.longitude)
     m = folium.Map(location=center_lat_lon, control_scale=True)
+
+    bound_lat_lons.append(center_lat_lon)
     folium.Marker(
         location=center_lat_lon,
-        tooltip="Current location",
+        tooltip="Center",
         icon=Icon(color="blue"),
     ).add_to(m)
 
-    for transportation_name, profile, fill_color in [
-        ("Driving", "driving-car", "blue"),
-        ("Cycling", "cycling-regular", "green"),
-        ("Walking", "foot-walking", "red"),
-    ]:
+    if home_coords is not None:
+        home_lat_lon = (home_coords.latitude, home_coords.longitude)
+        bound_lat_lons.append(home_lat_lon)
+        folium.Marker(
+            location=home_lat_lon,
+            tooltip="Home",
+            icon=Icon(color="green"),
+        ).add_to(m)
+
+    if work_coords is not None:
+        work_lat_lon = (work_coords.latitude, work_coords.longitude)
+        bound_lat_lons.append(work_lat_lon)
+        folium.Marker(
+            location=work_lat_lon,
+            tooltip="Work",
+            icon=Icon(color="yellow"),
+        ).add_to(m)
+
+    selected_isochrone_display_names = st.multiselect(
+        label="Plot isochrones", options=ISOCHRONE_OPTIONS.keys(), default=None
+    )
+    isochrone_minutes = st.number_input(
+        label="Isochrone minutes", step=15, min_value=15, max_value=120, value=30
+    )
+    isochrone_resl = []
+    for isochrone_display_name in selected_isochrone_display_names:
+        profile_name, isochrone_color = ISOCHRONE_OPTIONS[isochrone_display_name]
         isochrone_coords, isochrone_properties = get_isochrones(
             coords=center_coords,
-            range_seconds=60 * 30,
-            profile=profile,
+            range_seconds=60 * isochrone_minutes,
+            profile=profile_name,
         )
         isochrone_lat_lons = [
             [coords.latitude, coords.longitude] for coords in isochrone_coords
@@ -82,11 +133,17 @@ if center_coords is not None:
         folium.Polygon(
             locations=isochrone_lat_lons,
             weight=0,
-            fill_color=fill_color,
+            fill_color=isochrone_color,
             fill_opacity=0.2,
             fill=True,
-            tooltip=f"30mins {transportation_name} Reach Factor: {isochrone_properties['reachfactor']}",
+            tooltip=f"{isochrone_minutes} mins {isochrone_display_name} Reach Factor: {isochrone_properties['reachfactor']}",
         ).add_to(m)
+        isochrone_resl.append(
+            {
+                "Isochrone Type": isochrone_display_name,
+                **isochrone_properties,
+            }
+        )
 
     make_map_responsive = """
         <style>
@@ -94,4 +151,7 @@ if center_coords is not None:
         </style>
         """
     st.markdown(make_map_responsive, unsafe_allow_html=True)
+    m.fit_bounds(bound_lat_lons)
     folium_static(m, width=500, height=500)
+
+    st.write(pd.DataFrame(isochrone_resl))
--- src/geolocation.py
+++ src/geolocation.py
@@ -7,34 +7,43 @@ import requests_cache
 
 requests_cache.install_cache("geolocation_cache", backend="sqlite")
 ORS_KEY = os.getenv("OPENROUTESERVICE_API_KEY")
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass
 class Coords:
     latitude: float
     longitude: float
-    accuracy: Optional[float] = None
-    altitude: Optional[float] = None
-    altitudeAccuracy: Optional[float] = None
-    heading: Optional[float] = None
-    speed: Optional[float] = None
 
 
 def parse_get_geolocation(d: Optional[dict] = None) -> Optional[Coords]:
     """Parse output of streamlit_js_eval.get_geolocation."""
     if d is not None:
-        return Coords(**d.get("coords", {}))
+        return Coords(
+            latitude=d["coords"]["latitude"],
+            longitude=d["coords"]["longitude"],
+        )
 
 
-def get_geocode_top_hit(**kwargs) -> Coords:
+def get_geocode_top_hit(**kwargs) -> Optional[Coords]:
     params = {**kwargs}
     params.update({"api_key": ORS_KEY})
-    response = requests.get(
+    response_json = requests.get(
         "https://api.openrouteservice.org/geocode/search/", params=params
-    )
-    coords = response.json()["features"][0]["geometry"]["coordinates"]
-    longitude, latitude = coords[0], coords[1]
-    return Coords(latitude=latitude, longitude=longitude)
+    ).json()
+    try:
+        coords = response_json["features"][0]["geometry"]["coordinates"]
+        return Coords(latitude=coords[1], longitude=coords[0])
+    except Exception as e:
+        logger.warning(
+            f"Failed to get coordinates from geocode/search. response_json:\n{response_json}"
+            f"Error:\n{e}"
+        )
+
+
+ISOCHRONE_PROFILES = ["driving-car", "cycling-regular", "foot-walking", "wheelchair"]
 
 
 def get_isochrones(
@@ -42,12 +51,17 @@ def get_isochrones(
 ) -> Tuple[List[Coords], dict]:
     """
     # https://github.com/GIScience/openrouteservice-py/blob/9fc22f378db8f9ef98a1675031055b1ae2bec97b/openrouteservice/directions.py#L66-L69
-    profile: One of ["driving-car", "foot-walking", "cycling-regular"]
+    profile: One of ISOCHRONE_PROFILES
     """
+    if profile not in ISOCHRONE_PROFILES:
+        raise ValueError(
+            f"Profile {profile} not in ISOCHRONE_PROFILES: {ISOCHRONE_PROFILES}"
+        )
+    attributes = ["area", "reachfactor", "total_pop"]
     body = {
         "locations": [[coords.longitude, coords.latitude]],
         "range": [range_seconds],
-        "attributes": ["area", "reachfactor", "total_pop"],
+        "attributes": attributes,
     }
     headers = {
         "Accept": "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8",
@@ -65,5 +79,5 @@ def get_isochrones(
             Coords(longitude=l[0], latitude=l[1])
             for l in response.json()["features"][0]["geometry"]["coordinates"][0]
         ],
-        response.json()["features"][0]["properties"],
+        {k: response.json()["features"][0]["properties"][k] for k in attributes},
     )
